ac758ca6f36bf85d92e078acaaf732b1687d0cf1
Hugo30128 <104615072+Hugo30128@users.noreply.github.com>
2022-12-15 13:31:40 +0100

    début de css

--- POKEAPI/src/app/data.service.ts
+++ POKEAPI/src/app/data.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
 import { Observable, map, Subscription } from 'rxjs';
-=======
-import { map, Observable } from 'rxjs';
-import { Pokemon } from './pokemon.service';
-
->>>>>>> 78400be2c7e26179e6658f0393292689515732aa
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-<<<<<<< HEAD
     //BASE_URL = '/api'
     constructor( private httpClient: HttpClient ) { }
 
@@ -45,30 +38,3 @@ export class DataService {
         return this.pokemons.filter( el => el.name.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) >= 0 )
     }*/
 }
-=======
-
-  BASE_URL = '/api'
-
-  constructor(private http: HttpClient) { }
-
-  getPokemonsByGen1(gen: string): Observable<Pokemon[]> {
-    return this.http.get(this.BASE_URL + '/generation/3').pipe(
-      map( (data: any) => this.objArrayPokemon(data) )
-    )
-}
-
-
-protected objArrayPokemon(tab: any[] ): Pokemon[] {
-  const arr = tab[2]
-  return arr.map( (el: any) => {
-      const c: Pokemon = {
-          id: el.id[1],
-          name: el.name[1],
-      }
-      return c
-  })
-}
-}
-
-
->>>>>>> 78400be2c7e26179e6658f0393292689515732aa
