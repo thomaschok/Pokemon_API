021896dad98b7e5d445c89bcfa406c1c3e047002
thomaschok <thomas.choquell@isen.yncrea.fr>
2022-12-15 09:35:24 +0100

    test

--- POKEAPI/src/app/app.module.ts
+++ POKEAPI/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { Gen1Component } from './gen1/gen1.component';
 import { Gen1a3Component } from './gen1a3/gen1a3.component';
 import { Gen4a6Component } from './gen4a6/gen4a6.component';
 import { Gen7a8Component } from './gen7a8/gen7a8.component';
@@ -18,7 +17,6 @@ import { HeaderComponent } from './header/header.component';
     BodyComponent,
     HomeComponent,
     AboutComponent,
-    Gen1Component,
     Gen1a3Component,
     Gen4a6Component,
     Gen7a8Component,
