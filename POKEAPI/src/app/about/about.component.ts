022b0f69ad1936b68c1c19461ec0204d1db2a4c6
Hugo30128 <104615072+Hugo30128@users.noreply.github.com>
2022-12-15 14:59:37 +0100

    CSS ABOUT

--- POKEAPI/src/app/about/about.component.ts
+++ POKEAPI/src/app/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   teamMembers = [
     { nom: 'Corentin TYTECA', role: 'bg1' },
-    { nom: 'Thomas CHOKELL', role: 'bg2' },
+    { nom: 'Thomas CHOQUELL', role: 'bg2' },
     { nom: 'Hugo GOUIRAN', role: 'Le boss du CSS' },
     { nom: 'Gauthier LANNURIEN', role: 'stagiaire' }
   ];
