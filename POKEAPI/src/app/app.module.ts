52ba6adb36b53a865258125a706fa0b18517affc
thomaschok <thomas.choquell@isen.yncrea.fr>
2022-12-15 09:24:47 +0100

    Link

--- POKEAPI/src/app/app.module.ts
+++ POKEAPI/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { Gen1Component } from './gen1/gen1.component';
 import { Gen1a3Component } from './gen1a3/gen1a3.component';
 import { Gen4a6Component } from './gen4a6/gen4a6.component';
 import { Gen7a8Component } from './gen7a8/gen7a8.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { Gen7a8Component } from './gen7a8/gen7a8.component';
     Gen1Component,
     Gen1a3Component,
     Gen4a6Component,
-    Gen7a8Component
+    Gen7a8Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
