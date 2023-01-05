ea7a5115bc6e5106e1f88430c9db2132aa1523fe
Aimasu59 <corentintyteca@gmail.com>
2023-01-05 08:07:51 +0100

    pour hugo le rigolo

--- POKEAPI/src/app/app-routing.module.ts
+++ POKEAPI/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ObservablePageComponent } from './observable-page/observable-page.compo
 import { TeamComponent } from './team/team.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home'},
+  { path: '', pathMatch: 'full', redirectTo: '/Home'},
   {path: 'generation/:id',component: GenerationselectComponent},
   {path: 'team', component: TeamComponent},
   {path: 'Home', component: HomeComponent},
